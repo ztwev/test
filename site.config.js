6055cf5a2d59dd1e0051b972f39016acd4f9514e
ztwev <109983592+ztwev@users.noreply.github.com>
2024-01-29 17:37:28 -0500

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -53,7 +53,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
