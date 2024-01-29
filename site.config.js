e560d827fe2586a8ec6797bdd3b5eb3e114b2c65
ztwev <109983592+ztwev@users.noreply.github.com>
2024-01-29 18:50:20 -0500

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -23,8 +23,8 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://ztwev-blog.vercel.app",
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
